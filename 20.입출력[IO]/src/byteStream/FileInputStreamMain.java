73d1c0e2165c66f5887e90f0ab6486d2b20d9eeb
meltingong <hwk1210@naver.com>
2022-12-06 10:06:07 +0900

    ..

--- 20.입출력[IO]/src/byteStream/FileInputStreamMain.java
+++ 20.입출력[IO]/src/byteStream/FileInputStreamMain.java
@@ -11,11 +11,17 @@ import java.io.*;
  */
 public class FileInputStreamMain {
 
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args) throws Exception {
 		FileInputStream fis = new FileInputStream("fileOut.dat");
+		
 		/*
-		 * 
-		 */
+		public abstract int read() throws IOException
+			- Reads the next byte of data from the input stream. 
+			- The value byte is returned as an int in the range 0 to 255. 
+			- If no byte is available because the end of the stream has been reached, 
+			  the value -1 is returned.
+		  */
+		 
 		int readByte = fis.read();
 		System.out.println("1. byte : " + Integer.toBinaryString(readByte));
 		readByte = fis.read();
@@ -25,18 +31,18 @@ public class FileInputStreamMain {
 		readByte = fis.read();
 		System.out.println("4. byte : " + Integer.toBinaryString(readByte));
 		int byteCount = 0;
-		while(true) {
+		while (true) {
 			readByte = fis.read();
-			if(readByte == -1) {
+			if (readByte == -1) {
 				break;
 			}
 			byteCount++;
 			System.out.println(Integer.toBinaryString(readByte));
 		}
-		
+
 		fis.close();
 		System.out.println("FileInputStream.read() <-- fileOut.dat" + byteCount + "바이트 읽음");
-		
+
 	}
 
 }
