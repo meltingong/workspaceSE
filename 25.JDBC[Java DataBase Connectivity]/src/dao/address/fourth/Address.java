bb591a775f156f1ecf0e77634473205703643cc2
meltingong <hwk1210@naver.com>
2022-12-27 15:57:56 +0900

    ..

--- 25.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/Address.java
+++ 25.JDBC[Java DataBase Connectivity]/src/dao/address/fourth/Address.java
@@ -58,7 +58,7 @@ public class Address {
 	
 	@Override
 	public String toString() {
-		return "Address [no=" + no + ", name=" + name + ", phone=" + phone + ", address=" + address + "]";
+		return "Address [no=" + no + ", name=" + name + ", phone=" + phone + ", address=" + address + "]\n";
 	}
 	
 	
