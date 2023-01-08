943d8c9ea9665401567ee8f82c45af3135361124
meltingong <hwk1210@naver.com>
2023-01-08 15:49:59 +0900

    ..

--- java-project-class5-shop/src/com/itwill/shop/test/OrderDaoTestMain.java
+++ java-project-class5-shop/src/com/itwill/shop/test/OrderDaoTestMain.java
@@ -1,9 +1,16 @@
 package com.itwill.shop.test;
 
+import com.itwill.shop.order.OrderDao;
+
 public class OrderDaoTestMain {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		/*
+		 * 주문1개보기
+		 */
+		OrderDao orderDao = new OrderDao();
+		
+		System.out.println(orderDao.findByOrderNo("guard1",2));
 	}
 
 }
