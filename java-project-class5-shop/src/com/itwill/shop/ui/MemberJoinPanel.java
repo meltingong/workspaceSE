42684bc8b24614e7a20b637f97631a1baedbf748
meltingong <hwk1210@naver.com>
2023-01-10 18:03:25 +0900

    ..

--- java-project-class5-shop/src/com/itwill/shop/ui/MemberJoinPanel.java
+++ java-project-class5-shop/src/com/itwill/shop/ui/MemberJoinPanel.java
@@ -3,7 +3,6 @@ package com.itwill.shop.ui;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
-<<<<<<< HEAD
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -32,29 +31,6 @@ public class MemberJoinPanel extends JPanel {
 				frame.changePanel(ShopMainFrame.PRODUCT_LIST_PANEL,null);
 			}
 		});
-=======
 
-public class MemberJoinPanel extends JPanel {
-	ShopMainFrame frame;
-	/**
-	 * Create the panel.
-	 */
-	public MemberJoinPanel() {
-		setBackground(new Color(255, 153, 255));
-		setForeground(new Color(255, 51, 255));
-		setLayout(null);
-		
-		JButton btnNewButton = new JButton("상세보기화면전환");
-		btnNewButton.setBounds(61, 324, 158, 23);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("제품리스트화면전환");
->>>>>>> branch 'master' of https://github.com/meltingong/workspaceSE.git
-		btnNewButton_1.setBounds(61, 291, 158, 23);
-		add(btnNewButton_1);
-
-	}
-	public void setFrame(ShopMainFrame frame) {
-		this.frame = frame;
 	}
 }
